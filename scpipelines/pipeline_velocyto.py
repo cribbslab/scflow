bf8b5fe11e74e1782743d5dd5a457e781f642a96
Acribbs <adam.cribbs@imm.ox.ac.uk>
2019-05-23 08:38:01 +0100

    added star index function

--- scpipelines/pipeline_velocyto.py
+++ scpipelines/pipeline_velocyto.py
@@ -1,30 +1,3 @@
-##############################################################################
-#
-#   Botnar Resaerch Centre
-#
-#   $Id$
-#
-#   Copyright (C) 2018 Adam Cribbs
-#
-#   This program is free software; you can redistribute it and/or
-#   modify it under the terms of the GNU General Public License
-#   as published by the Free Software Foundation; either version 2
-#   of the License, or (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program; if not, write to the Free Software
-#   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-###############################################################################
-
-
-# To do:
-# merger reads if over multiple lanes
-
 """
 =================
 Pipeline velocyto
@@ -70,16 +43,10 @@ from ruffus import *
 import sys
 import os
 import sqlite3
-
-import cgatcore.pipeline as P
-import cgatcore.experiment as E
-import ModuleSC
-
 import pandas as pd
+
 import cgatcore.pipeline as P
 import cgatcore.experiment as E
-import ModuleSC
-
 import cgat.GTF as GTF
 import cgatcore.iotools as iotools
 
@@ -91,19 +58,6 @@ PARAMS = P.get_parameters(
      "pipeline.yml"])
 
 # Determine the location of the input fastq files
-
-try:
-    PARAMS['data']
-except NameError:
-    DATADIR = "."
-else:
-    if PARAMS['data'] == 0:
-        DATADIR = "."
-    elif PARAMS['data'] == 1:
-        DATADIR = "data.dir"
-    else:
-        DATADIR = PARAMS['data']
-
 SEQUENCESUFFIXES = ("*.fastq.gz",
 		    "*.fastq.1.gz")
 SEQUENCEFILES = tuple([os.path.join(DATADIR, suffix_name)
@@ -115,44 +69,42 @@ SEQUENCEFILES = tuple([os.path.join(DATADIR, suffix_name)
 ############################################
 
 
-@mkdir('geneset.dir')
+@active_if(PARAMS['star_index'] == 1)
+@mkdir('star_index.dir')
 @transform(PARAMS['geneset'],
-           regex("(\S+).gtf.gz"),
-           r"geneset.dir/\1.fa")
-def buildReferenceTranscriptome(infile, outfile):
-    '''
-    Builds a reference transcriptome from the provided GTF geneset - generates
-    a fasta file containing the sequence of each feature labelled as
-    "exon" in the GTF.
-    --fold-at specifies the line length in the output fasta file
-    Parameters
-    ----------
-    infile: str
-        path to the GTF file containing transcript and gene level annotations
-    genome_dir: str
-        :term: `PARAMS` the directory of the reference genome
-    genome: str
-        :term: `PARAMS` the filename of the reference genome (without .fa)
-    outfile: str
-        path to output file
-    '''
+           suffix(".gtf.gz"),
+           add_inputs(PARAMS['genome']),
+           "star_index.dir/Genome")
+def index_genome_star(infiles, outfile):
+    """
+    Generate a star index if no index is supplied
+    """
 
-    genome_file = os.path.abspath(
-        os.path.join(PARAMS["genome_dir"], PARAMS["genome"] + ".fa"))
+    gtffile, genome = infiles
 
-    statement = '''
-    zcat %(infile)s |
-    awk '$3=="exon"'|
-    cgat gff2fasta
-    --is-gtf --genome-file=%(genome_file)s --fold-at=60 -v 0
-    --log=%(outfile)s.log > %(outfile)s;
-    samtools faidx %(outfile)s
-    '''
+    statement = """STAR
+                   --runThreadN 12 
+                   --runMode genomeGenerate 
+                   --genomeDir star_index.dir/
+                   --genomeFastaFiles %(genome)s
+                   --sjdbGTFfile %(gtffile)s"""
 
     P.run(statement)
 
+@follows(PARAMS['star_index'] == 0)
+@mkdir('star_index.dir')
+@originate('star_index.dir/Genome')
+def index_genome_copy(outfile):
 
+    """
+    If a star index has already been generated then it will be copied
+    to the star_index.dir directory for tracability
+    """
 
+    star_location = PARAMS['star_index_dir']
+    statement = """cp -R %(star_location)s star_index.dir/ """
+
+    P.run(statement)
 
 @follows()
 def quant():
