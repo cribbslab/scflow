070e041f4964770504a642b3001e3b005dcde4a9
Anna James-Bott <anna.james-bott@st-hildas.ox.ac.uk>
2019-05-24 10:58:53 +0100

    Adding everything to scpipelines folder

---
+++ scpipelines/R/seurat.R
@@ -0,0 +1,31 @@
+#!/usr/bin/env Rscript
+
+library("tidyverse")
+library("SingleCellExperiment")
+library("optparse")
+library("Seurat")
+
+option_list = list(
+  make_option(c("-w", "--workingdir"), type="character", default=NULL, 
+              help="working directory", metavar="character"),
+  make_option(c("-i", "--input"), type="character", default="sce.rds", 
+              help="input file (sce.rds path) [default= %default]", metavar="character"),
+  make_option(c("-o", "--out"), type="character", default="seurat.rds", 
+              help="output file name [default= %default]", metavar="character")
+); 
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+wd = opt$workingdir
+
+setwd(wd)
+out <- opt$out
+input <- opt$input
+
+sce_object = readRDS(input)
+
+seurat_object = Convert(from = sce_object, to = "seurat")
+
+
+saveRDS(object = seurat_object, file = opt$out)
