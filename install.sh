10c8fe68a42c61fd70f2b6421c012aeaac7be349
Adam Cribbs <adam.cribbs@imm.ox.ac.uk>
2019-12-27 11:49:25 +0000

    try forcing install

--- install.sh
+++ install.sh
@@ -244,7 +244,7 @@ conda_install() {
 
     [[ -z ${TRAVIS_BRANCH} ]] && TRAVIS_BRANCH=${INSTALL_BRANCH}
     curl -o env.yml -O https://raw.githubusercontent.com/Acribbs/scflow/${TRAVIS_BRANCH}/conda/environments/${CONDA_INSTALL_TYPE}
-    conda env create --quiet --file env.yml
+    conda env create --quiet --force --file env.yml
 
     conda env export --name ${CONDA_INSTALL_ENV}
 
