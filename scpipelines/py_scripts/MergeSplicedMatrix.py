a360c1b15ba3effadce35adfb82b917c8db39ef1
Acribbs <adam.cribbs@imm.ox.ac.uk>
2020-02-05 16:22:37 -0800

    updated merge script with output

--- scpipelines/py_scripts/MergeSplicedMatrix.py
+++ scpipelines/py_scripts/MergeSplicedMatrix.py
@@ -9,14 +9,8 @@ import anndata
 import scipy.io
 import os
 import pandas as pd
+import scanpy as sc
 import cgatcore.experiment as E
-
-
-
-
-
-
-
 
 
 def main(argv=None):
@@ -28,10 +22,11 @@ def main(argv=None):
     parser.add_argument("-d", "--dir-bus", dest="bus_path", type=str,
                         help="The path to the bus directory")
 
-    parser.add_argument("-o", "--output", dest="", type=str,
+    parser.add_argument("-o", "--output", dest="output", type=str,
                         help="Output directory")
     parser.set_defaults(
         bus_path=None,
+        output=None,
     )
 
     (args) = E.start(parser)
@@ -48,7 +43,8 @@ spliced.obs= pd.read_csv('./bus_output/spliced/spliced.barcodes.txt', index_col 
 spliced.var = pd.read_csv('./bus_output/spliced/spliced.genes.txt', header = None, index_col = 0, names =['ensembl_id'], sep = '\t')
 print('Loaded spliced count matrix')
 
-# Now that we have spliced and unspliced matrices we can sum the counts of genes for barcodes common to both matrices We take the intersection of both matrices because presumably cells without a single count on either have very low counts anyway
+# Now that we have spliced and unspliced matrices we can sum the counts of genes for barcodes common to both matrices We take the intersection of both matrices
+# because presumably cells without a single count on either have very low counts anyway
 idx = spliced.obs.index.intersection(unspliced.obs.index)
 spliced_intersection = spliced[idx]
 spliced_intersection = unspliced[idx]
@@ -56,10 +52,10 @@ spliced_intersection.X + unspliced_intersection.X
 
 spliced_plus_unspliced = spliced_intersection.copy()
 spliced_plus_unspliced.X = spliced_intersection.X + unspliced_intersection.X
-spliced_plus_unspliced
+
 
 # Use scipy to write the matrix to .mtx file
-
+spliced_plus_unspliced.write(outfile)
 
 
 if __name__ == "__main__":
