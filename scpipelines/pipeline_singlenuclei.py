b849765d2cde597ff340f622611b04183824359a
Acribbs <adam.cribbs@imm.ox.ac.uk>
2020-02-05 16:51:57 -0800

    skeleton function for merging matrices

--- scpipelines/pipeline_singlenuclei.py
+++ scpipelines/pipeline_singlenuclei.py
@@ -206,7 +206,7 @@ def add_identifier(infile outfile):
 
 
 @merge([introns_transcripts,t2g],
-       r"geneset.dir/introns_t2g.txt")
+       "geneset.dir/introns_t2g.txt")
 def map_trans2gene(infiles, outfile):
     '''map the transcripts to their respective genes.'''
 
@@ -519,34 +519,14 @@ def bustools_count_cdna(infile, outfile):
     P.run(statement)
 
 
-#########################
-# SCE object
-#########################
+@merge([],
+    "geneset.dir/introns_t2g.txt")
+def merge_matrices(infile, outfile):
+    '''use python script to merge the spliced and unspliced matrix'''
 
-## Kallisto SCE object using BUSpaRse R package and emptydrops (DropletUtils function)
-@follows(mkdir("SCE.dir"))
-@follows(busText)
-@active_if(PARAMS['kallisto_bustools'])
-@transform(busText,
-           regex("kallisto.dir/(\S+)/bus/output.bus.sorted.txt"),
-           add_inputs(PARAMS['geneset'], getTranscript2GeneMap),
-           r"SCE.dir/\1/bus/sce.rds")
-def BUSpaRse(infiles, outfile):
-    '''
-    Create kallisto SCE object. Use BUSpaRse package to read in bus file and convert to TCC and gene counts matrix.
-    Create knee plot and use point of inflection to estimate number of empty droplets and cells.
-    Or use emptyDrops function from DropletUtils package to compare to the ambient profile.
-    '''
+    PYTHON_ROOT = os.path.join(os.path.dirname(__file__), "py_scripts")
 
-    bus_text, gtf, t2gmap = infiles
-    R_ROOT = os.path.join(os.path.dirname(__file__),"pipeline_singlecell","R")
-    est_cells = PARAMS['kallisto_expectedcells']
-
-    job_memory = '50G'
-
-    statement = '''
-    Rscript %(R_ROOT)s/BUSPaRse.R -i %(bus_text)s -o %(outfile)s --estcells %(est_cells)s --t2g %(t2gmap)s -g %(gtf)s
-    '''
+    statement = '''python %(PYTHON_ROOT)s/MergeSplicedMatrix.py -o %(outfile)s '''
 
     P.run(statement)
 
@@ -570,7 +550,7 @@ def build_multiqc(infile):
     P.run(statement)
 
 
-@follows(BUSpaRse, build_multiqc)
+@follows(, build_multiqc)
 def full():
     pass
 
