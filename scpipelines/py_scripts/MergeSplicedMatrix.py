8d505ebc5ae4dc2820eb1035fff5a8ab03a3ecc9
Acribbs <adam.cribbs@imm.ox.ac.uk>
2020-02-05 16:30:35 -0800

    added import files for script

--- scpipelines/py_scripts/MergeSplicedMatrix.py
+++ scpipelines/py_scripts/MergeSplicedMatrix.py
@@ -24,6 +24,24 @@ def main(argv=None):
 
     parser.add_argument("-o", "--output", dest="output", type=str,
                         help="Output directory")
+
+    parser.add_argument("-s", "--spliced", dest="spliced", type=str,
+                        help="The spliced .mtx file")
+
+    parser.add_argument("-c", "--sp-barcode", dest="spliced_barcode", type=str,
+                        help="The unspliced .mtx file")
+
+    parser.add_argument("-h", "--sp-genes", dest="spliced_genes", type=str,
+                        help="The unspliced .mtx file")
+
+    parser.add_argument("-u", "--unspliced", dest="unspliced", type=str,
+                        help="The unspliced .mtx file")
+
+    parser.add_argument("-b", "--unsp-barcode", dest="unspliced_barcode", type=str,
+                        help="The unspliced .mtx file")
+
+    parser.add_argument("-g", "--unsp-genes", dest="unspliced_genes", type=str,
+                        help="The unspliced .mtx file")
     parser.set_defaults(
         bus_path=None,
         output=None,
@@ -32,15 +50,15 @@ def main(argv=None):
     (args) = E.start(parser)
 
 ## load unspliced data on anndata as sparse crs matrix
-unspliced = anndata.AnnData(scipy.io.mmread('./bus_output/unspliced/unspliced.mtx').tocsr())
-unspliced.obs= pd.read_csv('./bus_output/unspliced/unspliced.barcodes.txt', index_col = 0, header = None, names = ['barcode'])
-unspliced.var = pd.read_csv('./bus_output/unspliced/unspliced.genes.txt', header = None, index_col = 0, names =['ensembl_id'], sep = '\t')
+unspliced = anndata.AnnData(scipy.io.mmread(unspliced).tocsr())
+unspliced.obs= pd.read_csv(unspliced_barcode, index_col = 0, header = None, names = ['barcode'])
+unspliced.var = pd.read_csv(unspliced_genes, header = None, index_col = 0, names =['ensembl_id'], sep = '\t')
 print('Loaded unspliced count matrix.')
 
 ## load unspliced data on anndata as sparse crs matrix
-spliced = anndata.AnnData(scipy.io.mmread('./bus_output/spliced/spliced.mtx').tocsr())
-spliced.obs= pd.read_csv('./bus_output/spliced/spliced.barcodes.txt', index_col = 0, header = None, names = ['barcode'])
-spliced.var = pd.read_csv('./bus_output/spliced/spliced.genes.txt', header = None, index_col = 0, names =['ensembl_id'], sep = '\t')
+spliced = anndata.AnnData(scipy.io.mmread(spliced).tocsr())
+spliced.obs= pd.read_csv(spliced_barcode, index_col = 0, header = None, names = ['barcode'])
+spliced.var = pd.read_csv(spliced_genes, header = None, index_col = 0, names =['ensembl_id'], sep = '\t')
 print('Loaded spliced count matrix')
 
 # Now that we have spliced and unspliced matrices we can sum the counts of genes for barcodes common to both matrices We take the intersection of both matrices
