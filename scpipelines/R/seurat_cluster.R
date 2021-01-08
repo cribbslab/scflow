4f173c1fb9dc2d949b5eee620e39805ee3fd69eb
annajbott <anna.james-bott@st-hildas.ox.ac.uk>
2021-01-08 16:00:11 +0000

    Rscript and rmarkdown task in pipeline. Script and rmarkdown mostly completed. Need to add HGNC symbols and test

--- scpipelines/R/seurat_cluster.R
+++ scpipelines/R/seurat_cluster.R
@@ -5,8 +5,92 @@ library(optparse)
 
 option_list <- list(
 		make_option(c("-i", "--input"), default=NULL,
-			help="The input rds file and path"),
+			help="The input rds file and path, filtered Seurat object"),
 		make_option(c("-s", "--sample"), default=NULL,
 			help="Sample name"),
-		make_option
-		
+		make_option(c("-v", "--variableFeatures"), default=2000, type="integer",
+			help="Number of variable features  [default %default]"),
+		make_option(c("--reddim"), default="pca",
+			help="What dimensionality reduction to use. PCA, harmony, zinbwave, CCA etc. [default %default]"),
+		  make_option(c("-d","--numdim"), default=NULL,
+              help="how many dimensions to use. Default to all calculated ones"),
+		  make_option(c("--resolution"), default=0.5,
+              help="Resolution for finding clusters. Sets the 'granularity' of the downstream clustering,
+              with increased values leading to a greater number of clusters. Between 0.4-1.2 good for ~3K cells.
+              [default %default]")
+)
+
+
+
+opt <- parse_args(OptionParser(option_list=option_list))
+
+
+sample_name <- read.table(opt$sample,header = TRUE, fill = T)
+num_variable_features <- read.table(opt$variableFeatures,header = TRUE, fill = T)
+num_dimensions <- opt$numdim
+reduction_technique <- opt$reddim
+resolution <- opt$resolution
+
+seurat_filtered_rds <- read.table(opt$input,header = TRUE, fill = T)
+seurat_unfiltered_rds <- gsub("_filtered_SeuratObject.rds", seurat_filtered, "unfiltered_SeuratObject.rds")
+
+# Read in RDS files (may take some time)
+filtered_seurat_object <- readRDS(seurat_filtered_rds)
+unfiltered_seurat_object <- readRDS(seurat_unfiltered_rds)
+
+# Normalise the data
+filtered_seurat_object <- NormalizeData(filtered_seurat_object)
+
+# Variable features (default = top 2000 genes)
+filtered_seurat_object <- FindVariableFeatures(filtered_seurat_object, selection.method = "vst", nfeatures = num_variable_features)
+
+# Identify the 10 most highly variable genes
+top10 <- head(VariableFeatures(filtered_seurat_object), 10)
+
+# plot variable features with and without labels
+plot1 <- VariableFeaturePlot(filtered_seurat_object)
+labelled_variable_feature_plot <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+
+# Scale the data- linear transformation prior to dimension reduction
+# (Might take some time)
+all.genes <- rownames(filtered_seurat_object)
+filtered_seurat_object <- ScaleData(filtered_seurat_object, features = all.genes)
+
+# Principal component analysis (PCA)
+filtered_seurat_object <- RunPCA(filtered_seurat_object, features = VariableFeatures(object = filtered_seurat_object))
+
+
+# How much to reduce
+filtered_seurat_object <- JackStraw(filtered_seurat_object, num.replicate = 100)
+filtered_seurat_object <- ScoreJackStraw(filtered_seurat_object, dims = 1:20
+
+jack_plot <- JackStrawPlot(filtered_seurat_object, dims = 1:15)
+name<- paste0("Clustering_Figures.dir/JackStrawPlot_", sample_name, ".eps")
+postscript(name)
+print(jack_plot)
+dev.off()
+
+elb_plt <- ElbowPlot(filtered_seurat_object)
+name<- paste0("Clustering_Figures.dir/ElbowPlot_", sample_name, ".eps")
+postscript(name)
+print(elb_plt)
+dev.off()
+
+# Number of dimensions to reduce to. Look at Jackdraw / elbow plots, if not user-defined runs Embeddings
+if(is.null(num_dimensions)){
+  reddim<-Embeddings(filtered_seurat_object, reduction.type = reduction_technique)
+  ncol(reddim)->maxN
+  dims.use <- seq(1,maxN,1)
+} else{
+  dims.use <- seq(1,num_dimensions,1) #change this if you want to use less components, or use the significant ones only (jackstraw procedure on PCA)
+}
+
+filtered_seurat_object <- FindNeighbors(filtered_seurat_object, dims = dims.use, reduction = reduction_technique)
+filtered_seurat_object <- FindClusters(filtered_seurat_object, resolution = resolution)
+
+saveRDS(filtered_seurat_object, gsub("SAMPLE_FILE",sample_name ,"RDS_objects.dir/SAMPLE_FILE_clustered_filtered_SeuratObject.rds"))
+
+# Non-linear cluster techniques, e.g. UMAP and tSNE.
+# Uses PCA to reduce dimensions.
+filtered_seurat_object <- RunUMAP(filtered_seurat_object, dims = dims.use)
+filtered_seurat_object <- RunTSNE(filtered_seurat_object, dims = dims.use)
--- scpipelines/pipeline_kb-cluster-3.py
+++ scpipelines/pipeline_kb-cluster-3.py
@@ -3,9 +3,9 @@
 Pipeline clustering
 ===================
 
-The pipeline follows pipeline_kb and pipeline_kb-filter-2
-and should be ran in the same directory as these pipelines
-
+The pipeline follows pipeline_kb, pipeline_kb-sampleqc-1 and pipeline_kb-filter-2
+It performs PCA, tSNE and UMAP dimensional reduction.
+Clustering is visualised in an Rmarkdown notebook.
 
 Authors
 =======
@@ -38,25 +38,55 @@ RMD_ROOT = os.path.join(os.path.dirname(__file__), "pipeline_kb-cluster-3","Rmar
 # R folder in main directory
 R_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__),"R"))
 
-# filtered files in format:
-# RDS_objects.dir/<SAMPLE>_filtered_SeuratObject.rds"
-rds_suffixes = "*_filtered_SeuratObject.rds"
+filtered_suffixes = "*_filtered_SeuratObject.rds"
 SEURAT_OBJECTS = tuple([os.path.join("RDS_objects.dir",filtered_suffixes)])
 
+@follows(mkdir("Clustering_Figures.dir"))
 @transform(SEURAT_OBJECTS,
 	regex("RDS_objects.dir/(\S+)_filtered_SeuratObject.rds"),
-	r"")
-def cluster(infile, outfile)
+	r"RDS_objects.dir/\1_clustered_filtered_SeuratObject.rds")
+def cluster(infile, outfile):
 	'''
-	Rscript task to run seurat clustering
+	R script task to run seurat clustering
 	'''
-	
+
 	file_name = os.path.basename(infile)
 	sample = re.match(r'(\S+)_filtered_SeuratObject.rds', file_name).group(1)
 
 	job_memory = "50G"
 
-	statement = Rscript %(R_PATH)s/seurat_cluster.R -i %(infile)s -s %(sample)s
+	statement = '''
+				Rscript %(R_PATH)s/seurat_cluster.R -i %(infile)s -s %(sample)s -v 2000
+				'''
 
 	P.run(statement)
 
+
+@follows(mkdir("Clustering_Figures.dir"))
+@merge(cluster,
+	"Cluster.html")
+def cluster_rmarkdown(infile, outfile):
+	'''
+	R markdown to visualise clustering and dimensional reduction
+	'''
+
+
+	job_memory = "50G"
+
+	statement = ''' cp %(RMD_ROOT)s/Cluster.Rmd . &&
+				R -e "rmarkdown::render('Cluster.Rmd', output_file='Cluster.html')"
+				'''
+
+	P.run(statement)
+
+@follows(cluster, cluster_rmarkdown)
+def full():
+	pass
+
+def main(argv=None):
+	if argv is None:
+		argv = sys.argv
+	P.main(argv)
+
+if __name__ == "__main__":
+	sys.exit(P.main(sys.argv))
