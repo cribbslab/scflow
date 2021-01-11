883531fe9aab835189802bdaf18ea4e1c05483d4
Acribbs <adam.cribbs@imm.ox.ac.uk>
2021-01-11 17:12:23 +0000

    fixed problem with barnyard plot

--- scpipelines/R/plot_barnyard.R
+++ scpipelines/R/plot_barnyard.R
@@ -8,11 +8,10 @@ library(optparse)
 
 option_list = list(
   make_option(c("-i", "--input"), type="character", default="", 
-              help="input dir", metavar="character"),
+              help="input file GTF file [default= %default]", metavar="character"),
   make_option(c("-o", "--out"), type="character", default="", 
-              help="output dir name [default= %default]", metavar="character"),
-	      );
- 
+              help="output file name [default= %default]", metavar="character")
+	      ); 
 
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
