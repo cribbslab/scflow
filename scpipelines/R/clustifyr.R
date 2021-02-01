f5e3194354916f1f0339405e00ea4bb0c523a6fa
annajbott <anna.james-bott@st-hildas.ox.ac.uk>
2021-02-01 13:06:15 +0000

    Dimension reduction technique parameterised, add ref_sce fucntion

--- scpipelines/R/clustifyr.R
+++ scpipelines/R/clustifyr.R
@@ -12,7 +12,9 @@ option_list <- list(
 		make_option(c("-s", "--sample"), default=NULL,
 			help="Sample name"),
         make_option(c("-r", "--reference"), default="reference_sce.rds", type = "character",
-			help="Location of reference sce rds file")
+			help="Location of reference sce rds file"),
+		make_option(c("-d", "--dimReduction"), default="umap", type = "character",
+			help="Dimension reduction technique, e.g. umap, pca, tsne.")
 )
 
 # Read in options
@@ -21,12 +23,24 @@ opt <- parse_args(OptionParser(option_list=option_list))
 input_file <- opt$input
 sample_name <- opt$sample
 reference_sce_loc <- opt$reference
+dim_red <- opt$dimReduction
 
 # Load seurat object
 seurat_object <- readRDS(input_file)
 
+#seurat_object_mat <- as.matrix(object_data(seurat_object, "data"))
+#metadata <- seurat_meta(seurat_object, dr = dr)
+
 # Load in reference sce object
 ref_sce <- readRDS("reference_sce_loc")
 ref_mat <- as.matrix(assay(ref_sce))
+colnames(ref_mat) <- ref_sce$label
 
-res <- clustify(input=seurat_object, ref_mat= ref_mat, cluster_col="seurat_clusters", obj_out=TRUE)
+# With their function
+new_ref_matrix_sce <- object_ref(
+  input = ref_sce,               # SCE object
+  cluster_col = "label"       # name of column in colData containing cell identities
+)
+
+
+res <- clustify(input=seurat_object, ref_mat= new_ref_matrix_sce, cluster_col="seurat_clusters", obj_out=TRUE, dr = dim_red )
