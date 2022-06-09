bcfe98bc99f3f607533909285d3323965e0f790b
2003100127 <jianfeng.sunmt@gmail.com>
2022-06-09 09:32:55 +0100

    comments 1

--- scpipelines/scanpy/pipeline_integ_1_qc.py
+++ scpipelines/scanpy/pipeline_integ_1_qc.py
@@ -23,43 +23,43 @@ PARAMS = P.get_parameters(
      "pipeline.yml"])
 print(PARAMS)
 
-# @mkdir('scanpy_integ_1_qc.dir')
-# def topy():
-#     '''
-#     convert a file from ipynb to py
-#     '''
-#     ROOT_DIR = os.path.join(os.path.dirname(__file__), 'pipeline_integ_1_qc')
-#
-#     statement = '''
-#     jupyter nbconvert --to python %(ROOT_DIR)s/h5.ipynb --output-dir=scanpy_integ_1_qc.dir
-#     '''
-#
-#     P.run(statement)
-#
-#
-# @mkdir('scanpy_integ_1_qc_fig.dir')
-# @follows(topy)
-# def run():
-#     ROOT_DIR = 'scanpy_integ_1_qc.dir'
-#
-#     job_memory = PARAMS['memory']
-#
-#     statement = '''
-#     python %(ROOT_DIR)s/h5.py --work_dir=./
-#     '''
-#     P.run(statement)
-#
-#
-# @follows(run)
-# def full():
-#     pass
-#
-#
-# def main(argv=None):
-#     if argv is None:
-#         argv = sys.argv
-#     P.main(argv)
-#
-#
-# if __name__ == "__main__":
-#     sys.exit(P.main(sys.argv))
+@mkdir('scanpy_integ_1_qc.dir')
+def topy():
+    '''
+    convert a file from ipynb to py
+    '''
+    ROOT_DIR = os.path.join(os.path.dirname(__file__), 'pipeline_integ_1_qc')
+
+    statement = '''
+    jupyter nbconvert --to python %(ROOT_DIR)s/h5.ipynb --output-dir=scanpy_integ_1_qc.dir
+    '''
+
+    P.run(statement)
+
+
+@mkdir('scanpy_integ_1_qc_fig.dir')
+@follows(topy)
+def run():
+    ROOT_DIR = 'scanpy_integ_1_qc.dir'
+
+    job_memory = PARAMS['memory']
+
+    statement = '''
+    python %(ROOT_DIR)s/h5.py --work_dir=./
+    '''
+    P.run(statement)
+
+
+@follows(run)
+def full():
+    pass
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    P.main(argv)
+
+
+if __name__ == "__main__":
+    sys.exit(P.main(sys.argv))
